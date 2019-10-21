983e7abf812284ac82ef1124b5ad5c666b8d33e6
dineshkumardg <dineshdg87@gmail.com>
2019-10-22 03:09:52 +0530

    for checkout purposes

--- appmap/appmap/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from resources.whatever_resource import WhateverResource
-
-whatever_resource = WhateverResource()
-
-urlpatterns = patterns('',
-   url(r'^api/', include(whatever_resource.urls)),
-)
--- appmap/dump.sql
+++
@@ -1,8 +0,0 @@
-use DolphinD;
-CREATE TABLE `appmap_whatever` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) NOT NULL,
-  `body` longtext NOT NULL,
-  `created_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2;
